40f4cefc3a7c9351cc0fa43c5ec3af0588e56b72
zjp292 <zjp29@nau.edu>
2023-12-04 17:07:12 -0700

    updated main code

--- weapon-system-support-software/weapon-system-support-software/MyForm.cpp
+++ weapon-system-support-software/weapon-system-support-software/MyForm.cpp
@@ -1,2 +1,11 @@
 #include "MyForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	weaponsystemsupportsoftware::MyForm form;
+	Application::Run(% form);
+}
