6e865f0f9f58c012045fabab55954e56cee2684a
zjp292 <zjp29@nau.edu>
2024-03-11 13:16:45 -0700

    added test cases
    added test cases:
    default constructor
    addNode
    
    I still need to update the name of the test file

--- Tests/tst_foo.cpp
+++ Tests/tst_foo.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include <QTest>
+#include "../weapon-system-support-software/electrical.cpp"
 
 // add necessary includes here
 
@@ -10,19 +11,66 @@ class foo : public QObject
 public:
     foo();
     ~foo();
+private:
+    electrical *electricalObject;
 
 private slots:
-    void test_case1();
+    void electrical_constructor();
+    void test_addNode();
 };
 
-foo::foo() {}
-
-foo::~foo() {}
-
-void foo::test_case1()
+foo::foo()
 {
-    QString str = "Hello";
-    QCOMPARE(str.toUpper(), QString("HELLO"));
+
+}
+
+foo::~foo()
+{
+}
+
+void foo::electrical_constructor()
+{
+    electrical *elecObj = new electrical();
+
+    // ensure that values are empty string or 0 for constructor
+    QCOMPARE(elecObj->name, "");
+    QCOMPARE(elecObj->voltage, 0);
+    QCOMPARE(elecObj->amps, 0);
+
+    // check the head/last node values
+    QCOMPARE(elecObj->headNode, nullptr);
+    QCOMPARE(elecObj->lastNode, nullptr);
+
+    delete elecObj;
+}
+
+void foo::test_addNode()
+{
+    // create electrical object and variables
+    electrical *elecObj = new electrical();
+    QString nodeName = "testName";
+    int voltage = 15;
+    int amps = 3;
+
+    elecObj->addNode(nodeName, voltage, amps);
+
+    // test the head/last node values
+    QVERIFY(elecObj->headNode != nullptr);
+    QVERIFY(elecObj->lastNode != nullptr);
+
+    // create a working node from the headNode
+    // should only be one node for test case
+    electricalNode *wkgNode = elecObj->headNode;
+
+    // test the values of the wkgnode
+    QVERIFY(wkgNode != nullptr);
+    QCOMPARE(wkgNode->name, nodeName);
+    QCOMPARE(wkgNode->voltage, voltage);
+    QCOMPARE(wkgNode->amps, amps);
+
+
+
+    delete elecObj;
 }
 
 QTEST_MAIN(foo)
