055afa03a249f8838492bb2ece750367f01b0104
Zachary Parham <zjp29@nau.edu>
2024-03-25 11:09:16 -0700

    completed loadData test
    added:
    - virtual deconstructor to make instance in test case work
    - included the h file in the executable in tests cmake
    - added devmode settings in WSSS cmake
    - added the test

--- Tests/tst_status.cpp
+++ Tests/tst_status.cpp
@@ -1,6 +1,6 @@
 #include <QCoreApplication>
 #include <QTest>
-//#include "../weapon-system-support-software/electrical.cpp"
+#include "../weapon-system-support-software/status.cpp"
 
 // add necessary includes here
 
@@ -23,8 +23,41 @@ private slots:
  */
 void tst_status::test_loadData()
 {
-    QString hello = "hello";
-    QCOMPARE(hello.toUpper(), "HELLO");
+    /* the statusMessage contains csv data in the following order
+     *
+        bool armed;
+        TriggerStatus trigger1; -> disengaged = 1, engaged = 0
+        TriggerStatus trigger2; -> disengaged = 1, engaged = 0
+        ControllerState controllerState; -> RUNNING=0, BLOCKED=1, TERMINATED=2, SUSPENDED=3
+        FiringMode firingMode; -> SAFE=0, SINGLE=1, BURST=2, FULL_AUTO=3
+        FeedPosition feedPosition;
+        //to find total events including errors, add total errors and events
+        int totalFiringEvents;
+        int burstLength;
+        double firingRate;
+     *
+     */
+
+    Status status;
+    QString dataMsg = "0,1,0,1,1,135,6,23,1636.14,\n";
+    QStringList values = dataMsg.split(DELIMETER);
+    int totalFiringEvents_testData = values[6].toInt();
+    int burstLength_testData = values[7].toInt();
+    double firingRate_testData = values[8].toDouble();
+
+    // call loadData function
+    status.loadData(dataMsg);
+
+    // verify the dataMsg passed tests
+    QVERIFY(status.armed == false);
+    QCOMPARE(status.trigger1, ENGAGED);
+    QCOMPARE(status.trigger2, DISENGAGED);
+    QCOMPARE(status.controllerState, BLOCKED);
+    QCOMPARE(status.firingMode, SINGLE);
+    QCOMPARE(status.feedPosition, FIRING);
+    QCOMPARE(status.totalFiringEvents, totalFiringEvents_testData);
+    QCOMPARE(status.burstLength, burstLength_testData);
+    QCOMPARE(status.firingRate, firingRate_testData);
 }
 
 /**
