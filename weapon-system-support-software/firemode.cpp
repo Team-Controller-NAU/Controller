8ca535ec392a09df086238af9f9cded02c9dca1e
Dylan-Motz <minidjsauce@gmail.com>
2024-04-15 04:58:12 -0700

    Issues #241, #206, #111

--- weapon-system-support-software/firemode.cpp
+++ weapon-system-support-software/firemode.cpp
@@ -43,19 +43,24 @@ void firemode::paintEvent(QPaintEvent*)
     // Smooth out the circle
     painter.setRenderHint(QPainter::Antialiasing);
 
+    //sets boarder color and thickness
     lines.setColor(QColor(255, 133, 51));
     lines.setWidth(3);
 
+    //sets painter to lines
     painter.setPen(lines);
 
+    //set brush color for background
     painter.setBrush(QBrush(QColor(27,27,27)));
 
+    //sets the area for the outline and background
     QRectF outlineArea(-QDial::height()/14, QDial::height()/22, QDial::height()*1.55, QDial::height()*1.55);
 
     //draws the outline
     painter.drawPie(outlineArea, -(62.5-90)*16, 125*16);
+    painter.drawLine(QDial::height()/1.44, QDial::height()/20,QDial::height()/1.44,QDial::height()/4.5);
+    //painter.drawLine(QDial::height()/5.2, QDial::height()/4,QDial::height()/3,QDial::height()/2.7);
 
-    painter.drawLine(QDial::height()/1.44, QDial::height()/20,QDial::height()/1.44,QDial::height()/5);
 
     // No border
     painter.setPen(QPen(Qt::NoPen));
@@ -75,14 +80,19 @@ void firemode::paintEvent(QPaintEvent*)
     //setting text
     painter.setPen(text);
 
+    //sets pen color for text
     painter.setPen(QPen(QColor(255, 255, 255))); // White color represented by RGB integers
 
+    //Initilize text font
     QFont textfont = painter.font();
 
+    //set font size
     textfont.setPixelSize(QDial::height()/10);
 
+    //set painter font
     painter.setFont(textfont);
 
+    //draws text
     painter.drawText(QDial::height()/13,QDial::height()/3,QDial::height(),QDial::height(),0,"Safe");
 
     painter.drawText(QDial::height()/3.5,QDial::height()/6.7,QDial::height(),QDial::height(),0,"Single");
@@ -91,8 +101,10 @@ void firemode::paintEvent(QPaintEvent*)
 
     painter.drawText(QDial::height()*1.13,QDial::height()/3,QDial::height(),QDial::height(),0,"Auto");
 
+    //set paint color for highlighted text
     painter.setPen(QPen(QColor(255, 133, 51))); // #ff8533 color represented by RGB integers
 
+    //draws colored text
     if(value == SAFE)
     {
         painter.drawText(QDial::height()/13,QDial::height()/3,QDial::height(),QDial::height(),0,"Safe");
@@ -110,7 +122,7 @@ void firemode::paintEvent(QPaintEvent*)
         painter.drawText(QDial::height()*1.13,QDial::height()/3,QDial::height(),QDial::height(),0,"Auto");
     }
 
-    //disable user edits on feed pos
+    //disable user edits on the needle
     setDisabled(true);
 }
 
