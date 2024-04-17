a92d6797d1112a107cdc1e6e51862792b3189e59
Zachary Parham <zjp29@nau.edu>
2024-04-17 11:13:12 -0700

    pushing to work on laptop

--- Tests/tst_file_system.cpp
+++ Tests/tst_file_system.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include <QTest>
+#include <QSettings>
 #include "../weapon-system-support-software/events.cpp"
 
 class tst_file_system : public QObject
@@ -12,8 +13,30 @@ private slots:
 
 void tst_file_system::tst_outputToLogFile()
 {
+    // initialize variables
+    QSettings userSettings("Team Controller", "WSSS");
     Events *eventObj = new Events();
+    int id = 15;
+    QString timeStamp = "0:00:00";
+    QString eventString = "tst_outputToLogFile";
+    QString logfile = userSettings.value("logfileLocation").toString();
+    QFile file(logfile + "/tst_outputToLogFile.txt");
 
+    // create event data and add to logfile
+    eventObj->addEvent(id, timeStamp, eventString);
+
+    eventObj->outputToLogFile(logfile + "/tst_outputToLogFile.txt", false);
+
+
+    // remove test file
+    if(file.remove())
+    {
+        qDebug() << "file removed";
+    }
+    else
+    {
+        qDebug() << "file not removed";
+    }
 
     delete eventObj;
 }
